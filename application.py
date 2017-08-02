3edf318669488336f3aa997bbb067d2f043bfc09
gusals3587 <gusals3587@gmail.com>
2017-08-02 19:33:34 +0900

    implemented index.html, and some minor changes to application.py

--- application.py
+++ application.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_login import login_user, logout_user, current_user, login_required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' #TODO
 db = SQLAlchemy(app)
 
-login_manager
-
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text, unique=True)
     password = db.Column(db.Text, unique=True)
@@ -24,14 +20,14 @@ def index():
 
 @app.route("/signUp", methods=["POST"])
 def register():
-    if request.form["username"] = "" or request.form["password"] = "":
+    if request.form["username"] == "" or request.form["password"] == "":
         return render_template("sign_up.html")
     user = User(request.form["username"], request.form["password"])
     db.session.add(user)
     db.session.commit()
     return render_template("success.html")
 
-@app.route("/login", methods=["POST", "GET"]):
+@app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "GET":
         return render_template("index.html")
